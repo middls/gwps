da9504d1dceac3938bb6c4241dbbcafdfc288793
middls <middls647@gmail.com>
2019-09-24 18:38:14 +0300

    верстка секции autonomy

---
+++ src/components/autonomy/autonomy.js
@@ -0,0 +1,13 @@
+import Plugin from "@/scripts/core/Plugin";
+import init from "@/scripts/core/init";
+import Vue from 'vue';
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+export default {
+  components: {
+    swiper,
+    swiperSlide
+  }
+}
+
+export default init(Autonomy, "autonomy");
